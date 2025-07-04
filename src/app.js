235e1b262eea1cdc9766767f3ad5edf743811aec
LoretoConcha <109807122+LoretoConcha@users.noreply.github.com>
2025-07-04 21:52:17 +0000

    Domain name generator with nested for

--- src/app.js
+++ src/app.js
@@ -2,10 +2,31 @@ import "bootstrap";
 import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+
+let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  // let hacks = ['com', 'net','us', 'io'];
+
+
+// for(let i=0; i<pronoun.length;i++){
+//   for(let j=0; i<adj.length;j++){
+//     for(let k=0; i<noun.length; k++){
+//       let domainConcatenado=pronoun[i]+adj[j]+noun[k];
+//       console.log()
+//     }
+//   }
+// }
+
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+for(let i=0; i<pronoun.length;i++){
+  for(let j=0; i<adj.length;j++){
+    for(let k=0; i<noun.length; k++){
+      let domainConcatenado=pronoun[i]+adj[j]+noun[k];
+      console.log(domainConcatenado)
+    }
+  }
+}
+console.log("Hello Rigo from the console!");
 };
