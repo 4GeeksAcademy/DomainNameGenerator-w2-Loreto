733c58db75da65d55b914b72752d6e33cff333e5
LoretoConcha <109807122+LoretoConcha@users.noreply.github.com>
2025-07-04 22:31:33 +0000

    Domain

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-  let pronoun = ['the', 'our'];
+let pronoun = ['the', 'our'];
 let adj = ['great', 'big'];
 let noun = ['jogger', 'racoon'];
   
--- src/app.js
+++ src/app.js
@@ -4,9 +4,9 @@ import "./style.css";
 
 
 let pronoun = ['the', 'our'];
-  let adj = ['great', 'big'];
-  let noun = ['jogger', 'racoon'];
-  // let hacks = ['com', 'net','us', 'io'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+// let hacks = ['com', 'net','us', 'io'];
 
 
 // for(let i=0; i<pronoun.length;i++){
@@ -19,14 +19,14 @@ let pronoun = ['the', 'our'];
 // }
 
 
-window.onload = function() {
-for(let i=0; i<pronoun.length;i++){
-  for(let j=0; i<adj.length;j++){
-    for(let k=0; i<noun.length; k++){
-      let domainConcatenado=pronoun[i]+adj[j]+noun[k];
-      console.log(domainConcatenado)
+window.onload = function () {
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        let domainConcatenado = pronoun[i] + adj[j] + noun[k]+".com";
+        console.log(domainConcatenado)
+      }
     }
   }
-}
-console.log("Hello Rigo from the console!");
+  console.log("Hello Rigo from the console!");
 };
